4b00ec00df4d0f4bda91b555016701fa5565327a
tswartzendruber <travswartzenduber@gmail.com>
2024-02-06 16:44:30 -0600

    Added comments to SignInOrSignUp html and js

--- FrontEnd/SignInOrSignUp.js
+++ FrontEnd/SignInOrSignUp.js
@@ -1,7 +1,12 @@
 function showUsernameAndPassword() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+    /* This function simply displays the username and password entered onto the screen */
+    /* This function is intended for testing purposes only, and will not be included in the actual site */
 
-    var testingDiv = document.getElementById("testingDiv");
+    var username = document.getElementById("username").value;   // Read input from text input with id="username"
+    var password = document.getElementById("password").value;   // Read input from text input with id="password"
+
+    var testingDiv = document.getElementById("testingDiv");     // Locate the testing div in HTML file
+
+    // Add two paragraph elements containing the username and password entered by the user
     testingDiv.innerHTML = "<p>Username: " + username + "</p><p>Password: " + password + "</p>";
 };
