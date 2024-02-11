bdf42967d261b216441e57633759fa8edb524d51
tswartzendruber <travswartzenduber@gmail.com>
2024-02-11 17:33:27 -0600

    Added structure and styling for sign in page

--- FrontEnd/SignInOrSignUp.js
+++ FrontEnd/SignInOrSignUp.js
@@ -10,3 +10,16 @@ function showUsernameAndPassword() {
     // Add two paragraph elements containing the username and password entered by the user
     testingDiv.innerHTML = "<p>Username: " + username + "</p><p>Password: " + password + "</p>";
 };
+
+
+function togglePasswordVisibility() {
+    /* This function allows the user to show/hide their password on the screen (password hidden by default) */
+    
+    var passwordInput = document.getElementById("password");    // Locate the password text entry
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";        // Show the user's password
+    } else {
+        passwordInput.type = "password";    // Hide the user's password
+    }
+};
