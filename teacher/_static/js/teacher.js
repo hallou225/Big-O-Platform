d3bc5d9f938c18e06ef5f981aa463fd363d4207b
tswartzendruber <travswartzenduber@gmail.com>
2024-03-09 11:15:19 -0600

    Setting up for sidebar styling in teacher console

--- teacher/_static/js/teacher.js
+++ teacher/_static/js/teacher.js
@@ -21,15 +21,3 @@ function createNewClass() {
     window.open("./createclass", "_self");
 
 }
-
-function togglePasswordVisibility() {
-    /* This function allows the user to show/hide their password on the screen (password hidden by default) */
-    
-    var passwordInput = document.getElementById("password");    // Locate the password text entry
-
-    if (passwordInput.type === "password") {
-        passwordInput.type = "text";        // Show the user's password
-    } else {
-        passwordInput.type = "password";    // Hide the user's password
-    }
-}
