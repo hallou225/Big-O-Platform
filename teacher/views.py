055f871586557329d2cc7ed6ed833eb785d60011
tswartzendruber <travswartzenduber@gmail.com>
2024-03-15 10:29:53 -0500

    Updated teacher views and create class page

--- teacher/views.py
+++ teacher/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url="/login")
 def teacher(request):
     return render(request, 'teacher.html')
 
+@login_required(login_url="/login")
 def createclass(request):
     return render(request, 'createclass.html')
 
+@login_required(login_url="/login")
 def profile(request):
     return render(request, 'profile.html')
