26715156fe5dae601d3b637ef13f5ba5ec5259d0
hallou <111776211+hallou225@users.noreply.github.com>
2024-03-18 23:01:43 -0500

    added db

---
+++ _database/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import *
+
+admin.site.register(Role)
+admin.site.register(Account)
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Language)
+admin.site.register(Term)
+admin.site.register(Class)
+admin.site.register(Module)
