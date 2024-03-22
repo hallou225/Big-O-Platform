21c0d34ab572b45466774e9e2c6a9205b564ba6f
hallou <111776211+hallou225@users.noreply.github.com>
2024-03-21 23:03:59 -0500

    Updated algorithm upload page

--- bigo/settings.py
+++ bigo/settings.py
@@ -83,6 +83,11 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+                
+        'libraries':{
+            'custom_filters': 'bigo.templatetags.custom_filters',
+            
+            }
         },
     },
 ]
