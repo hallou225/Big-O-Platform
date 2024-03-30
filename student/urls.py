19425116560d7bbef7e1ece425e69af894e34565
tswartzendruber <travswartzenduber@gmail.com>
2024-03-30 17:11:25 -0500

    View contents of classes & modules, leave classes

--- student/urls.py
+++ student/urls.py
@@ -5,11 +5,12 @@ urlpatterns = [
     path("", views.student, name="student"),
     path("joinclass/", views.joinclass, name="joinclass"),
     path("profile/", views.profile, name="profile"),
+    path("class/<str:class_pk>/", views.studentClass, name="studentClass"),
+    path("class/<str:class_pk>/leaveClass/", views.leaveClass, name="leaveClass"),
+    path("class/<str:class_pk>/module/<str:module_pk>/", views.studentModule, name="studentModule"),
 ]
 
 '''
-path("class/<str:class_pk>/", views.studentClass, name="studentClass"),
-path("class/<str:class_pk>/leaveClass/", views.leaveClass, name="leaveClass"),
 path("class/<str:class_pk>/module/<str:module_pk>/", views.module, name="module"),
 path("class/<str:class_pk>/module/<str:module_pk>/algorithm/", views.algorithm, name="algorithm"),
 '''
