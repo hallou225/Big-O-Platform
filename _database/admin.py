126a092908cba06114996f56320baeec949d26db
tswartzendruber <travswartzenduber@gmail.com>
2024-03-30 14:36:03 -0500

    Started student side, able to join class from site

--- _database/admin.py
+++ _database/admin.py
@@ -5,6 +5,8 @@ from django.contrib import admin
 from .models import *
 
 
+class ClassAdmin(admin.ModelAdmin):
+    filter_horizontal = ('students',)  # This will display the students as a multiple select widget
 
 admin.site.register(Role)
 admin.site.register(Account)
@@ -12,12 +14,7 @@ admin.site.register(Teacher)
 admin.site.register(Student)
 admin.site.register(Language)
 admin.site.register(Term)
-admin.site.register(Class)
+admin.site.register(Class, ClassAdmin)
 admin.site.register(Module)
 admin.site.register(Algorithm)
 admin.site.register(Line)
-
-"""
-
-
-"""
--- student/views.py
+++ student/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from bigo.forms import *
 
 # Create your views here.
 
@@ -15,6 +16,74 @@ def isStudent(request):
 
 @login_required(login_url="/login")
 def student(request):
+    print("Student Views: def student(request):")
     if not isStudent(request):
         return redirect("/login")
     return render(request, 'student.html')
+
+@login_required(login_url="/login")
+def profile(request):
+    print("Student Views: def profile(request):")
+    if not isStudent(request):
+        return redirect("/login")
+    
+    student = request.user
+    context = {"student": student}
+
+    return render(request, 'student_profile.html', context)
+
+@login_required(login_url="/login")
+def joinclass(request):
+    print("Student Views: def joinclass(request):")
+    if not isStudent(request):
+        return redirect("/login")
+    
+    errorMessage = ""
+    form = JoinClassForm()
+
+    if request.method == "POST":
+        form = JoinClassForm(request.POST)
+        if form.is_valid():
+            try:
+                class_code = form.cleaned_data["class_code"]
+                class_to_join = Class.objects.get(class_code=class_code)
+                student = request.user
+                class_to_join.students.add(student)
+                print("Redirect to other page......")
+                return redirect("/student")
+            except Class.DoesNotExist:
+                print("No class with that code......")
+                errorMessage = "There is no class with this class code."
+
+
+    context = {"form": form, "errorMessage": errorMessage}
+    return render(request, 'joinclass.html', context)
+
+
+@login_required(login_url="/login")
+def joinclass(request):
+    print("Student Views: def joinclass(request):")
+    if not isStudent(request):
+        return redirect("/login")
+    
+    errorMessage = ""
+    form = JoinClassForm()
+
+    if request.method == "POST":
+        form = JoinClassForm(request.POST)
+
+        class_code = request.POST.get("class_code")
+        
+        classesWithCode = Class.objects.filter(class_code=class_code)
+        
+        if classesWithCode.exists():
+            classToJoin = classesWithCode.first()
+            student = request.user._wrapped
+            classToJoin.students.add(student)
+            errorMessage = "Students in this class: "
+            return redirect("/student")
+        else:
+            errorMessage = "There is no class with this class code."
+
+    context = {"form": form, "errorMessage": errorMessage}
+    return render(request, 'joinclass.html', context)
