71d99f7f86c348f040dcc4ea9eeddc365573d061
tswartzendruber <travswartzenduber@gmail.com>
2024-03-30 17:11:25 -0500

    View contents of classes & modules, leave classes

--- student/views.py
+++ student/views.py
@@ -19,7 +19,23 @@ def student(request):
     print("Student Views: def student(request):")
     if not isStudent(request):
         return redirect("/login")
-    return render(request, 'student.html')
+
+    student_id = request.user.id
+    student_username = request.user.username
+    student_password = request.user.password
+    student_display_name = request.user.display_name
+    student_first_name = request.user.first_name
+    student_classes = request.user.student_class.all()
+
+    context = {
+        "student_id": student_id,
+        "student_username": student_username,
+        "student_password": student_password,
+        "student_display_name": student_display_name,
+        "student_first_name":student_first_name,
+        "student_classes": student_classes
+    }
+    return render(request, 'student.html', context)
 
 @login_required(login_url="/login")
 def profile(request):
@@ -33,32 +49,16 @@ def profile(request):
     return render(request, 'student_profile.html', context)
 
 @login_required(login_url="/login")
-def joinclass(request):
-    print("Student Views: def joinclass(request):")
+def studentClass(request, class_pk):
+    print("Student Views: def studentClass(request):")
     if not isStudent(request):
         return redirect("/login")
-    
-    errorMessage = ""
-    form = JoinClassForm()
+        
+    student_class = Class.objects.get(id=class_pk)
+    modules = student_class.module_set.all()
 
-    if request.method == "POST":
-        form = JoinClassForm(request.POST)
-        if form.is_valid():
-            try:
-                class_code = form.cleaned_data["class_code"]
-                class_to_join = Class.objects.get(class_code=class_code)
-                student = request.user
-                class_to_join.students.add(student)
-                print("Redirect to other page......")
-                return redirect("/student")
-            except Class.DoesNotExist:
-                print("No class with that code......")
-                errorMessage = "There is no class with this class code."
-
-
-    context = {"form": form, "errorMessage": errorMessage}
-    return render(request, 'joinclass.html', context)
-
+    context = {"student_class": student_class, "modules": modules}
+    return render(request, 'studentClass.html', context)
 
 @login_required(login_url="/login")
 def joinclass(request):
@@ -79,11 +79,46 @@ def joinclass(request):
         if classesWithCode.exists():
             classToJoin = classesWithCode.first()
             student = request.user._wrapped
-            classToJoin.students.add(student)
-            errorMessage = "Students in this class: "
-            return redirect("/student")
+
+            if student in classToJoin.students.all():
+                errorMessage = "You are already enrolled in this class."
+            else:
+                classToJoin.students.add(student)
+                return redirect("/student")
         else:
             errorMessage = "There is no class with this class code."
 
     context = {"form": form, "errorMessage": errorMessage}
     return render(request, 'joinclass.html', context)
+
+def leaveClass(request, class_pk):
+    print("Student Views: def leaveClass(request):")
+    if not isStudent(request):
+        return redirect("/login")
+        
+    student_class = Class.objects.get(id=class_pk)
+
+    if request.method == "POST":
+        student_class.students.remove(request.user)
+        return redirect("/student")
+
+    context = {"student_class": student_class}
+    return render(request, 'leaveClass.html', context)
+
+
+
+
+
+
+
+@login_required(login_url="/login")
+def studentModule(request, class_pk, module_pk):
+    print("Student Views: def studentModule(request):")
+    if not isStudent(request):
+        return redirect("/login")
+
+    student_class = Class.objects.get(id=class_pk)
+    module = Module.objects.get(id=module_pk)
+    context = {"student_class": student_class, "module": module}
+
+    return render(request, 'studentModule.html', context)
