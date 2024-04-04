f0239ba90492d27a5f293a9f520be97effaed4c1
tswartzendruber <travswartzenduber@gmail.com>
2024-04-04 00:41:19 -0500

    Updated login using bootstrap

--- login/_static/js/login.js
+++ login/_static/js/login.js
@@ -1,7 +1,21 @@
+/*
 function togglePasswordVisibility() {
-    /* This function allows the user to show/hide their password on the screen (password hidden by default) */
+    // This function allows the user to show/hide their password on the screen (password hidden by default) /
     
     var passwordInput = document.getElementById("password");    // Locate the password text entry
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";        // Show the user's password
+    } else {
+        passwordInput.type = "password";    // Hide the user's password
+    }
+}
+*/
+
+function togglePasswordVisibility() {
+    // This function allows the user to show/hide their password on the screen (password hidden by default) /
+    
+    var passwordInput = document.getElementById("form_password");    // Locate the password text entry
 
     if (passwordInput.type === "password") {
         passwordInput.type = "text";        // Show the user's password
