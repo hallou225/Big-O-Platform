786cb841ce9f02fea5268416dc938da444e7d60f
hallou <111776211+hallou225@users.noreply.github.com>
2024-04-08 17:05:58 -0500

    added test page and content on teacher's upload algorithm

--- login/urls.py
+++ login/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("login/", views.loginPage, name="login"),
     path("logout/", views.logoutUser, name="logout"),
     path("bootstrap/", views.bootstrap, name="bootstrap"),
+    path("test_code/", views.test_code, name="test_code"),
 ]
